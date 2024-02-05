c45d9811cdae005cb431414c1cc501303d588ef8
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2024-02-05 14:04:18 +0700

    save config data  in Azure Key Vault

--- config.py
+++ config.py
@@ -1,38 +1,58 @@
 import os
+from sqlalchemy.engine import URL
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+# Adapted from the 
+vaulturl = f'https://article-vaults.vault.azure.net'
+credential = DefaultAzureCredential()
+secret = SecretClient(vault_url= vaulturl, credential= credential)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or secret.get_secret('blob-account').value
+    # BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'f4hu/yM/9+Xff3BJPnTXykP1wMHggwStABMRVT9aLCGpB93p/3OyxqkkrWzgmKRnUzHGdtX1J07M+ASt+fjDUQ=='
+    STORAGE_CONNECTION_STRING = secret.get_secret('storage-connection-string').value
+    
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or secret.get_secret('blob-container-images').value
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or secret.get_secret('sql-server-url').value
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or secret.get_secret('sql-server-database').value
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or secret.get_secret('sql-username').value
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or secret.get_secret('sql-password').value
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = URL.create(
+        'mssql+pyodbc',
+        username=SQL_USER_NAME,
+        password=SQL_PASSWORD,
+        host=SQL_SERVER,
+        port=1433,
+        database=SQL_DATABASE,
+        query= {
+            "driver": "ODBC Driver 17 for SQL Server"
+        }
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
 
-    ### Info for MS Authentication ###
+    ### Info for MS Authentication ###pyodbc
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
+    CLIENT_SECRET = secret.get_secret('authen-secret').value
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
     # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-    # if not CLIENT_SECRET:
-    #     raise ValueError("Need to define CLIENT_SECRET environment variable")
+    if not CLIENT_SECRET:
+        raise ValueError("Need to define CLIENT_SECRET environment variable")
 
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = secret.get_secret('authen-client-id').value
 
-    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "/getAuthToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
